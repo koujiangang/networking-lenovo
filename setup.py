0150062b58a7949e954b142e05a4c79e303a3509
Will.liu <liuxw9@lenovo.com>
2018-02-27 03:50:59 -0500

    add Lenovo/Openstack copyright

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
 #
+# Copyright (c) 2017, Lenovo. All rights reserved.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
